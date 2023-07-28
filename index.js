3d71634c79f48b3e8b224e0a88ec58a84b300036
Lahiru Supun <lhrsupun@gmail.com>
2023-07-28 17:43:00 +0530

    push

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 
 const ftp = require("basic-ftp")
 require('dotenv').config();
+
+let client;
+const bytesToMb = (bytes) => `${(bytes / (1024 * 1024)).toFixed(2)} MB`;
+
 async function example() {
-    const client = new ftp.Client()
-    client.ftp.verbose = true
+    client = new ftp.Client()
+    // client.ftp.verbose = true
     try {
         const {
             FTP_HOST, FTP_USER, FTP_PASSWORD, FTP_PORT
         } = process.env
+        client.trackProgress(info => {
+            console.log("File:", info.name)
+            console.log("Type:", info.type)
+            console.log("Transferred:", bytesToMb(info.bytes))
+            console.log("Transferred Overall:", bytesToMb(info.bytesOverall))
+        })
         await client.access({
             host: FTP_HOST,
             user: FTP_USER,
@@ -18,8 +28,16 @@ async function example() {
                 rejectUnauthorized: false
             }
         });
-        const data = await client.list();
-        console.log({ data });
+        const files = await client.list('/S3/assests/');
+        // console.log({ files: data.map(f => f?.name) });
+        // const files = data.map(f => f?.name);
+        for (const file of files) {
+            await client.downloadToDir(`./files/${file?.name}`, `/S3/assests/${file?.name}`);
+        }
+        // const path = await client.cd('/S3/new-build');
+        // console.log({ path });
+        // await Promise.all(data.map(f => client.downloadToDir(`./files/${f?.name}`, `/S3/assests/${f?.name}`)));
+        // await client.downloadToDir('./files/','/S3/new-build/g0086');
     }
     catch (err) {
         console.log({ err });
@@ -28,3 +46,12 @@ async function example() {
 }
 
 example();
+
+process.on('SIGINT', gracefulShutdown);
+process.on('SIGTERM', gracefulShutdown);
+
+function gracefulShutdown() {
+    console.log('Shutting down gracefully...');
+    client.close();
+    process.exit(0);
+}
